1af166408f733d5d4bf30794bc5463390532e9d3
Elijah Williams <ejwill04@gmail.com>
2017-02-02 16:03:09 -0700

    Hooked up favorites to container

--- app/containers/PopularMoviesContainer.js
+++ app/containers/PopularMoviesContainer.js
@@ -1,13 +1,17 @@
 import { connect } from 'react-redux';
 import PopularMovies from '../components/popularMovies';
-import { addPopularMovies } from '../actions';
+import { toggleFavorite } from '../actions';
 
 const mapStateToProps = (state) => {
   return { popularMovies: state.popularMovies.results, user: state.user };
 };
 
-// const mapDispatchToProps = (dispatch) => {
-//   return;
-// };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleFavorite: (data, favoriteId) => {
+      dispatch(toggleFavorite(data, favoriteId));
+    },
+  };
+};
 
-export default connect(mapStateToProps, null)(PopularMovies);
+export default connect(mapStateToProps, mapDispatchToProps)(PopularMovies);
--- app/reducers/popularMoviesReducer.js
+++ app/reducers/popularMoviesReducer.js
@@ -2,6 +2,19 @@ const popularMovies = (state = [], action) => {
   switch (action.type) {
     case 'ADD_POPULAR_MOVIES':
       return action.data;
+    case 'TOGGLE_FAVORITE':
+      const newState = state.results.map((movie, index) => {
+        if (movie.id === action.data.id) {
+          if (movie.favorited) {
+            return Object.assign({}, action.data, { favorited: false }, { favoriteId: -1 });
+          } else {
+            return Object.assign({}, action.data, { favorited: true }, { favoriteId: action.favoriteId });
+          }
+        } else {
+          return movie;
+        }
+      });
+      return Object.assign({}, state, { results: newState });
     default:
       return state;
   }
