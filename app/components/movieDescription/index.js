b5868530f1922bf8fa50571284b6380e30d06218
Adam Rice <adamcoltonrice@gmail.com>
2017-02-03 16:46:25 -0700

    styling on movie description for individual movie

--- app/components/movieDescription/index.js
+++ app/components/movieDescription/index.js
@@ -15,18 +15,21 @@ const MovieDescription = (props) => {
   const { id, title, poster_path, overview, favorited, release_date, backdrop_path, vote_average, vote_count } = movie;
   return (
     <div className='movie-desc'>
-      <h2 className='movie-desc-title'>{movie.title}</h2>
-      <div className='movie-desc-vote-avg'>Rating: {vote_average}/10</div>
-      <p className='movie-desc-release-date'>Release Date: {release_date}</p>
-      <div className='bg'>
-        <img className='movie-desc-backdrop' src={`https://image.tmdb.org/t/p/w500/${backdrop_path}`} />
-      </div>
       <img
         className='movie-desc-poster'
         src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
         alt={`movie-poster-${title}`}
       />
-      <p className='movie-desc-overview'>{overview}</p>
+      <div className='bg'>
+        <img className='movie-desc-backdrop' src={`https://image.tmdb.org/t/p/w500/${backdrop_path}`} />
+      </div>
+      <div className='movie-text-data'>
+        <p className='movie-desc-title'>{movie.title}</p>
+        <p className='movie-desc-vote-avg'>Rating: {vote_average}/10</p>
+        <p className='movie-desc-release-date'>Release Date: {release_date}</p>
+        <p className='movie-desc-overview'>{overview}</p>
+
+      </div>
 
     </div>
   );
