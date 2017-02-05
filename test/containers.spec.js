ea6a9f1c5fd24642554f39903e21f6bd16032b03
Adam Rice <adamcoltonrice@gmail.com>
2017-02-05 14:09:08 -0700

    passing test that Provider is rendering MoviesContainer and MoviesComponent

--- test/containers.spec.js
+++ test/containers.spec.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import sinon from 'sinon';
 import { expect } from './setup';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import configureMockStore from 'redux-mock-store';
 import Movies from '../app/components/movies';
 import * as actions from '../app/actions';
+import { Provider } from 'react-redux';
+import MoviesContainer from '../app/containers/MoviesContainer';
 const fakeStore = configureMockStore()({ movies: [] });
 
 describe('containers', () => {
@@ -17,6 +18,17 @@ describe('containers', () => {
           [{ id: 1 }]
         )]
       );
+    });
+
+    it('should render', () => {
+      const wrapper = mount(
+        <Provider store={fakeStore}>
+          <MoviesContainer />
+        </Provider>);
+      const Component = wrapper.find(MoviesContainer);
+      const MoviesComponent = Component.find(Movies);
+      expect(Component).to.exist;
+      expect(MoviesComponent).to.exist;
     });
   });
 });
