3851cb3cf9b861b665e8a418420f6918e853ae52
Adam Rice <adamcoltonrice@gmail.com>
2017-02-05 13:47:16 -0700

    tests that movies container dispatches an action, passing

--- test/containers.spec.js
+++ test/containers.spec.js
@@ -1,6 +1,22 @@
-import configureMockStore from 'redux-mock-store';
 import React from 'react';
-import { shallow, mount } from 'enzyme';
 import sinon from 'sinon';
 import { expect } from './setup';
+import { shallow, mount } from 'enzyme';
+import configureMockStore from 'redux-mock-store';
+import Movies from '../app/components/movies';
+import * as actions from '../app/actions';
 const fakeStore = configureMockStore()({ movies: [] });
+
+describe('containers', () => {
+  describe('Movies', () => {
+    it('dispatches actions', () => {
+      fakeStore.dispatch(actions.addMovies([{ id: 1 }]));
+      const fakeActions = fakeStore.getActions();
+      expect(fakeActions).to.deep.equal(
+        [actions.addMovies(
+          [{ id: 1 }]
+        )]
+      );
+    });
+  });
+});
