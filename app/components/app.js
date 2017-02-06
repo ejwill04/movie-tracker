9f48ddfad1ca2fd33b2405772e1bcbef28d6c2c9
Eric Sayler <easayler@gmail.com>
2017-02-06 09:06:00 -0700

    update title font weight

--- app/components/app.js
+++ app/components/app.js
@@ -69,7 +69,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Film Favs</h1>
+        <h1 className='title'><span className='title-film'>Film</span>Favs</h1>
         {this.toggleBtnPath()}
         {this.showLoginBtn()}
         {this.props.children}
